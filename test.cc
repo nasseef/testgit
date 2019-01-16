9880a1694c5411cc0ee0dde6957126b3ba0df12f
Nasseef Abukamail <abukamai@ohio.edu>
2019-01-15 22:37:38 -0500

    improved test.cc

---
+++ test.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: test.cc
+ *      Author: Nasseef Abukamail
+ *        Date: January 12, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+   cout << "Hello Git!!" << endl;
+   cout << "Base feature" << endl;
+   return 0;
+}// main
